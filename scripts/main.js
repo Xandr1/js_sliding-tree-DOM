f40b361b753b464af1803cc559412176eafe74b0
Oleksandr Kirsanov <olek.kirsanov@gmail.com>
2019-10-28 15:47:19 +0200

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', function(targetEvent) {
+  if (event.target.className !== 'element') {
+    return;
+  }
+  const ulChild = event.target.parentNode.querySelector('ul');
+  if (ulChild) {
+    ulChild.hidden = !ulChild.hidden;
+  }
+});
